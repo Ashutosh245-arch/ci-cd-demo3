f15aadf0ac752f5562a77ee0c3ba26e2c50ad546
Ashutosh Dhoka <dhokashutosh70@gmail.com>
2025-05-29 13:06:40 +0530

    First commit

---
+++ src/main/java/com/example/demo/CiCdDemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdDemo3Application.class, args);
+	}
+
+}
